1609611425a3d528e866bbfffa6e1f0c6bd8ad8e
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2020-04-04 20:01:21 +0530

    [factory-pattern] - Add loggers

---
+++ README.md
@@ -0,0 +1,3 @@
+# Design-Patterns
+
+Factory design pattern is a cretional design pattern. That means controll the Object creation in the system by Hiding the Object creation process.
--- src/main/java/factory_pattern/driver_class/DriverClass.java
+++ src/main/java/factory_pattern/driver_class/DriverClass.java
@@ -4,35 +4,58 @@ import factory_pattern.employees_concrete.BusinessAnalyst;
 import factory_pattern.employees_concrete.QAEngineer;
 import factory_pattern.employees_concrete.SoftwareEngineer;
 import factory_pattern.employees_interface.Employee;
+import factory_pattern.exceptions.EmployeeTypeNotFoundException;
 import factory_pattern.factory.EmployeeFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DriverClass {
+    private static Logger logger = LoggerFactory.getLogger(DriverClass.class);
+
     public static void main(String[] args) {
         /**
          * to create a software engineer object without using new keyword
          * needed to type cast as software engineer
          */
-        SoftwareEngineer softwareEngineer = (SoftwareEngineer) EmployeeFactory.getEmployee(Employee.SOFTWARE_ENGINEER);
-        System.out.println("software engineer name :"+softwareEngineer.getName());
-        System.out.println("software engineer age :"+softwareEngineer.getAge());
-        System.out.println("software engineer salary :"+softwareEngineer.getSalary());
+        SoftwareEngineer softwareEngineer = null;
+        try {
+            softwareEngineer = (SoftwareEngineer) EmployeeFactory.getEmployee(Employee.SOFTWARE_ENGINEER);
+            logger.info("software engineer name :" + softwareEngineer.getName());
+            logger.info("software engineer age :" + softwareEngineer.getAge());
+            logger.info("software engineer salary :" + softwareEngineer.getSalary());
+        } catch (EmployeeTypeNotFoundException e) {
+            String message = "Check the Software engineer type. You have entered a invalid type";
+            logger.error(message, e);
+        }
 
         /**
          * to create a business analyst object without using new keyword
          * needed to type cast as business analyst
          */
-        BusinessAnalyst businessAnalyst = (BusinessAnalyst) EmployeeFactory.getEmployee(Employee.BUSINESS_ANALYST);
-        System.out.println("business analyst name :"+businessAnalyst.getName());
-        System.out.println("business analyst age :"+businessAnalyst.getAge());
-        System.out.println("business analyst salary :"+businessAnalyst.getSalary());
+        BusinessAnalyst businessAnalyst = null;
+        try {
+            businessAnalyst = (BusinessAnalyst) EmployeeFactory.getEmployee(Employee.BUSINESS_ANALYST);
+            logger.info("business analyst name :" + businessAnalyst.getName());
+            logger.info("business analyst age :" + businessAnalyst.getAge());
+            logger.info("business analyst salary :" + businessAnalyst.getSalary());
+        } catch (EmployeeTypeNotFoundException e) {
+            String message = "Check the business analyst type. You have entered a invalid type";
+            logger.error(message, e);
+        }
 
         /**
          * to create a qa engineer object without using new keyword
          * needed to type cast as qa engineer
          */
-        QAEngineer qaEngineer = (QAEngineer) EmployeeFactory.getEmployee(Employee.QA_ENGINEER);
-        System.out.println("qa engineer name :"+qaEngineer.getName());
-        System.out.println("qa engineer age :"+qaEngineer.getAge());
-        System.out.println("qa engineer salary :"+qaEngineer.getSalary());
+        QAEngineer qaEngineer = null;
+        try {
+            qaEngineer = (QAEngineer) EmployeeFactory.getEmployee(Employee.QA_ENGINEER);
+            logger.info("qa engineer name :" + qaEngineer.getName());
+            logger.info("qa engineer age :" + qaEngineer.getAge());
+            logger.info("qa engineer salary :" + qaEngineer.getSalary());
+        } catch (EmployeeTypeNotFoundException e) {
+            String message = "Check the qa engineer type. You have entered a invalid type";
+            logger.error(message, e);
+        }
     }
 }
