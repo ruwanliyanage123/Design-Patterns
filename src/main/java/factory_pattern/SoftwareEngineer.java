310b24e4309edab5bce6c04cd1decebe645c8aba
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2020-04-04 19:09:42 +0530

    Add software engineer class

---
+++ src/main/java/factory_pattern/SoftwareEngineer.java
@@ -0,0 +1,37 @@
+package factory_pattern;
+
+public class SoftwareEngineer implements Employee {
+    private int age;
+    private String name;
+    private double salary;
+
+    public SoftwareEngineer(int age, String name, double salary) {
+        this.age = age;
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
