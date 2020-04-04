9035368b84fd6dad3df93afee03db7dcdfbfb77a
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2020-04-04 19:26:34 +0530

    Add factory class

--- src/main/java/factory_pattern/Employee.java
+++ src/main/java/factory_pattern/Employee.java
@@ -1,5 +1,7 @@
 package factory_pattern;
 
 public interface Employee {
-
+    public static final String SOFTWARE_ENGINEER = "SoftwareEngineer";
+    public static final String QA_ENGINEER = "QAEngineer";
+    public static final String BUSINESS_ANALYST = "BusinessAnalyst";
 }
