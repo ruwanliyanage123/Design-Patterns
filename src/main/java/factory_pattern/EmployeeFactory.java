d843248a2c85bde7deaee5d8e14e191150d0726b
ruwanliyanage123 <ruwanliyanagegalle1994@gmail.com>
2020-04-04 19:26:34 +0530

    [factory-pattern] - Add factory class

---
+++ src/main/java/factory_pattern/EmployeeFactory.java
@@ -0,0 +1,21 @@
+package factory_pattern;
+
+public class EmployeeFactory {
+    public static Employee getEmployee(String employeeType){
+        Employee employee;
+        switch (employeeType){
+            case Employee.SOFTWARE_ENGINEER:
+                employee = new SoftwareEngineer(26,"Kamal",175000);
+                break;
+            case Employee.BUSINESS_ANALYST:
+                employee = new BusinessAnalyst(31,"Nisal",165000);
+                break;
+            case Employee.QA_ENGINEER:
+                employee = new QAEngineer(23,"Dasun",125000);
+                break;
+            default:
+                throw new IllegalArgumentException();
+        }
+        return employee;
+    }
+}
