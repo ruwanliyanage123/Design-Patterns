fbcea25b492180148a7f305e3d8cfa88d61d0b34
ruwanliyanage123 <ruwanliyanagegalle1994@gmaill.com>
2020-05-09 14:33:03 +0530

    [proxy-pattern] - Add ActualVideo class

---
+++ src/main/java/proxy_pattern/ActualVideo.java
@@ -0,0 +1,32 @@
+package proxy_pattern;
+
+public class ActualVideo implements Video{
+    private String path;
+
+    /**
+     * Will call download method to connect with the server
+     * @param path Url to the video
+     */
+    public ActualVideo(String path){
+        this.path = path;
+        this.downLoadVideo(path);
+    }
+
+    /**
+     * This method used to download video from the server
+     * In here proxy server will contain the video
+     * @param path Url to the video
+     */
+    public void downLoadVideo(String path){
+        System.out.println("Downloading..."+path);
+    }
+
+    /**
+     * Will display the video
+     * @param path Url to the video
+     */
+    @Override
+    public void display(String path) {
+        System.out.println("This is the video from real server"+path);
+    }
+}
