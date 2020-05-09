5ec229420eca7ee7c26d95c49cbf1652fe19e516
ruwanliyanage123 <ruwanliyanagegalle1994@gmaill.com>
2020-05-09 09:40:09 +0530

    [proxy-pattern] - Add Video interface

---
+++ src/main/java/proxy_pattern/Video.java
@@ -0,0 +1,9 @@
+package proxy_pattern;
+
+/**
+ * public interface for the video
+ * Actual video and Proxy video will implements this interface
+ */
+public interface Video {
+    public void display(String path);
+}
